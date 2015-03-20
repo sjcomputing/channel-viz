a3f3cf0d01ef9903f76b6f4ccdfb8cbdea09a8c4
sjcomputing <steve@sjcomputing.net>
2015-03-20 09:16:23 +0000

    Update follows.js

--- follows.js
+++ follows.js
@@ -16,9 +16,9 @@
 		defaultFeeds	= [87872], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'SJComputing', // Replaces Xively logo in the header
 		dataDuration	= '6hours', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
-		dataInterval	= 60, // Default interval for data to be displayed (in seconds)
-		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
-		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
+		dataInterval	= 10800, // Default interval for data to be displayed (in seconds)
+		dataColor		= '0A1922', // CSS HEX value of color to represent data (omit leading #)
+		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
 
